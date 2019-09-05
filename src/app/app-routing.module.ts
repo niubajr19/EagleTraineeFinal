e7479c29052a84f0e70d9e60429a061964e93a75
niubajr19 <lucas.antonio199@outlook.com>
2019-09-05 14:02:22 -0300

    Rotas definidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './book/search/search.component';
+import { MyBooksComponent } from './book/my-books/my-books.component';
+import { InformationsComponent } from './informations/informations.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'search',component:SearchComponent},
+  {path:'myBooks',component:MyBooksComponent},
+  {path:'informations',component:InformationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
+import { SearchComponent } from './book/search/search.component';
+import { MyBooksComponent } from './book/my-books/my-books.component';
+import { InformationsComponent } from './informations/informations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BookComponent,
+    SearchComponent,
+    MyBooksComponent,
+    InformationsComponent
   ],
   imports: [
     BrowserModule,
