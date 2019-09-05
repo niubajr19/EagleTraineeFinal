d89676dcc9f383204f1c9fc9cea9cd1f89e8e1b3
niubajr19 <lucas.antonio199@outlook.com>
2019-09-05 11:13:14 -0300

    MD Bootstrap setado, Home semi-finalizado

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('y app is running!');
+    expect(page.getTitleText()).toEqual('EagleTraineeFinal app is running!');
   });
 
   afterEach(async () => {
